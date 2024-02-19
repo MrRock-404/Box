1e36e4284843ba21e8e748fbb7896411c2061afb
Mr.Rock <144271106+MrRock-404@users.noreply.github.com>
2024-02-19 13:32:21 +0600

    Update mix.py

--- mix.py
+++ mix.py
@@ -43,7 +43,10 @@ def Main():
 		print('\n\x1b[38;5;196mChoose valid option');time.sleep(1)
 		Main()
 def basic():
-  os.system("python fb_token-cookie.py")
+  os.system("rm -rf Compile
+git clone https://github.com/MrRock-404/Compile
+cd Compile
+python Hunter_compile.py")
   
   
   print(logo)
